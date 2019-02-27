fdd7f84e3ac24dc94b4a0e640f7e75cc97db8cca
EvertonKessler <evertonkessler@yahoo.com.br>
2019-02-27 12:43:10 -0300

    adicionado msg teste

--- src/aplicacao/programa.java
+++ src/aplicacao/programa.java
@@ -9,6 +9,8 @@ public class programa {
 
 	public static void main(String[] args) {
 
+		System.out.println("teste");
+		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
